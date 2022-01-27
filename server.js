667347d5fc0539a4d6143901faf072f8e848d9c8
Cole Hendershot <colehendershot@gmail.com>
2022-01-27 14:17:15 -0700

    Trying to link rollbar

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ const app = express()
 // app.use(express.json())
 app.use(cors())
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'ade276e79ec44f198913263fa6fbf41b',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 // This part is specific to Heroku/more basic hosting options.
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "./index.html"));
@@ -15,6 +26,8 @@ app.get("/", function(req, res) {
 
 // process.env.PORT is HEROKU'S responsibility. They will provide the PORT number on deployment.
 // The second port number is for local development purposes only.
+app.use(rollbar.errorHandler());
+
 const port = process.env.PORT || 4005;
 
 app.listen(port, () => {
